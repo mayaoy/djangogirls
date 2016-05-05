cc0d83caed5748d550a2d052c0d0713463e152a8
MayaOY <may61016125@gmail.com>
2016-05-05 19:02:18 +0800

    for try dj.static setting,

--- mysite/mysite/production_settings.py
+++ mysite/mysite/production_settings.py
@@ -10,6 +10,7 @@ DATABASES = {
 
 # Static asset configuration.
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
